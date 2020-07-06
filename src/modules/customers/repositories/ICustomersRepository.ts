064ba5f02bf4538db77c65c1b013d1c54f1b780f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-06 09:14:12 -0300

    Create customer repository

---
+++ src/modules/customers/repositories/ICustomersRepository.ts
@@ -0,0 +1,8 @@
+import Customer from '../infra/typeorm/entities/Customer';
+import ICreateCustomerDTO from '../dtos/ICreateCustomerDTO';
+
+export default interface ICustomersRepository {
+  create(data: ICreateCustomerDTO): Promise<Customer>;
+  findByEmail(email: string): Promise<Customer | undefined>;
+  findById(id: string): Promise<Customer | undefined>;
+}
