7cc7e7f7d4a1b983b3204cef4189e8e52ab57799
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-05 23:08:12 -0300

    Create server

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import AppError from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
+import routes from './routes';
+
+import '@shared/container';
+
+createConnection();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export default app;
