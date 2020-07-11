3c37a5b6a7d3f2859af187067261c2c15f31778e
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-11 20:48:53 -0300

    Create orders service

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,71 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import Order from '../infra/typeorm/entities/Order';
+import IOrdersRepository from '../repositories/IOrdersRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+@injectable()
+class CreateOrderService {
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const orderProducts = await this.productsRepository.findAllById(products);
+
+    const serializedProducts = products.map(({ id, quantity }) => {
+      const orderProduct = orderProducts.find(product => product.id === id);
+
+      if (!orderProduct) {
+        throw new AppError('Product not found');
+      }
+
+      if (quantity > orderProduct.quantity) {
+        throw new AppError('There is not enough product quantity in stock');
+      }
+
+      return {
+        product_id: id,
+        price: orderProduct.price,
+        quantity,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: serializedProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
+  }
+}
+
+export default CreateOrderService;
