86ec62de2e78210bcac5a8b68f0d8def238af764
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-11 20:46:50 -0300

    Create products service

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,37 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Product from '../infra/typeorm/entities/Product';
+import IProductsRepository from '../repositories/IProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+@injectable()
+class CreateProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productExists = await this.productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('A product with the same name already exists');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+    return product;
+  }
+}
+
+export default CreateProductService;
