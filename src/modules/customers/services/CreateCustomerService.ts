dbb956b7b6fe290630e56375ecf67d61fe3b5e52
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-10 23:14:22 -0300

    Create customer controller and service

---
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Customer from '../infra/typeorm/entities/Customer';
+import ICustomersRepository from '../repositories/ICustomersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+@injectable()
+class CreateCustomerService {
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ name, email }: IRequest): Promise<Customer> {
+    const customerExists = await this.customersRepository.findByEmail(email);
+
+    if (customerExists) {
+      throw new AppError('The email provided already in use');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
+  }
+}
+
+export default CreateCustomerService;
