915d234ffca18f27c7e05252091b0cb15279e6de
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-11 20:47:09 -0300

    Create products repository

---
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,73 @@
+import { getRepository, Repository, In } from 'typeorm';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
+
+class ProductsRepository implements IProductsRepository {
+  private ormRepository: Repository<Product>;
+
+  constructor() {
+    this.ormRepository = getRepository(Product);
+  }
+
+  public async create({
+    name,
+    price,
+    quantity,
+  }: ICreateProductDTO): Promise<Product> {
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+    await this.ormRepository.save(product);
+    return product;
+  }
+
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
+  }
+
+  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
+    const foundProducts = await this.ormRepository.find({
+      where: {
+        id: In(products.map(({ id }) => id)),
+      },
+    });
+
+    return foundProducts;
+  }
+
+  public async updateQuantity(
+    products: IUpdateProductsQuantityDTO[],
+  ): Promise<Product[]> {
+    const orderProducts = await this.findAllById(
+      products.map(({ id }) => ({ id })),
+    );
+
+    const updatedProducts = orderProducts.map(product => {
+      const orderProduct = products.find(({ id }) => id === product.id);
+      let { quantity } = product;
+
+      if (orderProduct) {
+        quantity -= orderProduct.quantity;
+      }
+
+      return { ...product, quantity };
+    });
+
+    await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
+  }
+}
+
+export default ProductsRepository;
