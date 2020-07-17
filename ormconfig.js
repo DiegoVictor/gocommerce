fedd36022dad114cab89f05ceca9a56f245a9055
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-16 21:35:50 -0300

    Change ormconfig to js

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/shared/infra/typeorm/migrations',
+  },
+};
