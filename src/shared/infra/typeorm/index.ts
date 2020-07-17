9d8593d8b683abe66b6e48f840fc8dc02af20403
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-17 09:10:09 -0300

    Change test database name

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -8,8 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'gostack_desafio09_tests'
-          : defaultOptions.database,
+          ? 'tests'
     }),
   );
 };
