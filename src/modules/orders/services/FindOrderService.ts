49c2b541127561bf0e1a268f85b7129192a56915
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-20 08:27:29 -0300

    Remove not used dependencies

--- src/modules/orders/services/FindOrderService.ts
+++ src/modules/orders/services/FindOrderService.ts
@@ -1,7 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-import IProductsRepository from '@modules/products/repositories/IProductsRepository';
-import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
 import AppError from '@shared/errors/AppError';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
@@ -15,12 +13,6 @@ class FindOrderService {
   constructor(
     @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
-
-    @inject('ProductsRepository')
-    private productsRepository: IProductsRepository,
-
-    @inject('CustomersRepository')
-    private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ id }: IRequest): Promise<Order> {
