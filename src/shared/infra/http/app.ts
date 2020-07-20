eea46aa2fa88285fcbb51dd31f68e9d70e5543c3
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-20 08:21:08 -0300

    Add route version

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use('/v1', routes);
 
 app.use(errors());
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
