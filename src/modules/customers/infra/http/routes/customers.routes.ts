53ac309533934228f28be61b09bc3aa9e088ea5b
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-20 08:26:53 -0300

    Validate custmer creation

--- src/modules/customers/infra/http/routes/customers.routes.ts
+++ src/modules/customers/infra/http/routes/customers.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import CustomersController from '../controller/CustomersController';
+import CustomerValidator from '../validators/CustomerValidator';
 
 const customersRouter = Router();
 const customersController = new CustomersController();
 
-customersRouter.post('/', customersController.create);
+customersRouter.post('/', CustomerValidator, customersController.create);
 
 export default customersRouter;
