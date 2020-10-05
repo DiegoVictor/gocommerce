036fddc274f6a0ca9e27742191e90db162ebb580
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-05 20:10:17 -0300

    Set express request type

---
+++ src/@types/express.d.ts
@@ -0,0 +1,7 @@
+declare namespace Express {
+  // eslint-disable-next-line @typescript-eslint/interface-name-prefix
+  export interface Request {
+    host_url: string;
+    current_url: string;
+  }
+}
