a28c282859a46a3917d2e896f3a4de203dd95265
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-11 14:43:52 -0300

    Validate id

--- src/modules/orders/infra/http/routes/orders.routes.ts
+++ src/modules/orders/infra/http/routes/orders.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
-import OrdersController from '../controller/OrdersController';
-import OrderValidator from '../validators/OrderValidator';
+import IdValidator from '@shared/infra/http/validators/IdValidator';
+import OrdersController from '@modules/orders/infra/http/controller/OrdersController';
+import OrderValidator from '@modules/orders/infra/http/validators/OrderValidator';
 
 const ordersRouter = Router();
 const ordersController = new OrdersController();
 
 ordersRouter.post('/', OrderValidator, ordersController.create);
-ordersRouter.get('/:id', ordersController.show);
+ordersRouter.get('/:id', IdValidator, ordersController.show);
 
 export default ordersRouter;
