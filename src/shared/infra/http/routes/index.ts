b79e0789f3545bdf8007c9b99bc86fb4c5b4e50d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-11 14:41:01 -0300

    Configure rate limit to all routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,8 +3,11 @@ import { Router } from 'express';
 import customersRouter from '@modules/customers/infra/http/routes/customers.routes';
 import productsRouter from '@modules/products/infra/http/routes/products.routes';
 import ordersRouter from '@modules/orders/infra/http/routes/orders.routes';
+import RateLimit from '@shared/infra/http/middlewares/RateLimit';
 
 const routes = Router();
+
+routes.use(RateLimit);
 
 routes.use('/customers', customersRouter);
 routes.use('/products', productsRouter);
