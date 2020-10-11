8316d8cd1529c56553a0e6be551713b37f4aace4
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-11 14:40:38 -0300

    Create rate limit storage

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,5 +1,33 @@
 import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 
+import { createClient, RedisClient } from 'redis';
+import {
+  RateLimiterMemory,
+  RateLimiterRedis,
+  RateLimiterAbstract,
+  IRateLimiterOptions,
+} from 'rate-limiter-flexible';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createClient as mockClient } from 'redis-mock';
+
+import config from '../../config/redis';
+
+const redis = ((): RedisClient => {
+  if (process.env.NODE_ENV === 'test') {
+    return mockClient();
+  }
+
+  return createClient(config);
+})();
+
+export function RateLimiter(opts: IRateLimiterOptions): RateLimiterAbstract {
+  if (process.env.NODE_ENV === 'test') {
+    return new RateLimiterMemory(opts);
+  }
+
+  return new RateLimiterRedis({ storeClient: redis, ...opts });
+}
+
 export default async (name = 'default'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
