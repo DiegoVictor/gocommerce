fb762f9a26baf01940782a167445211c61dca8d9
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-11 14:38:38 -0300

    Type error class

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,11 +1,18 @@
+interface IInternalData {
+  [key: string]: number | string;
+}
+
 class AppError {
   public readonly message: string;
 
   public readonly statusCode: number;
 
-  constructor(message: string, statusCode = 400) {
+  public readonly data: IInternalData;
+
+  constructor(message: string, data: IInternalData, statusCode = 400) {
     this.message = message;
     this.statusCode = statusCode;
+    this.data = data;
   }
 }
 
