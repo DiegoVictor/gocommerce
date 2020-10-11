67d4907bb0531e925ff6f3140f761b18c8d0837f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-11 14:43:02 -0300

    Apply HATEOAS

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -6,12 +6,25 @@ import FindOrderService from '@modules/orders/services/FindOrderService';
 
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
+    const { current_url, host_url } = request;
     const { id } = request.params;
 
     const findOrder = container.resolve(FindOrderService);
     const order = await findOrder.execute({ id });
 
-    return response.json(order);
+    return response.json({
+      ...order,
+      customer: {
+        ...order.customer,
+        url: `${host_url}/v1/customers/${order.customer_id}`,
+      },
+      order_products: order.order_products.map(product => ({
+        ...product,
+        url: `${host_url}/v1/products/${product.id}`,
+      })),
+      products_url: `${host_url}/v1/orders/${order.id}/products`,
+      url: current_url,
+    });
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
