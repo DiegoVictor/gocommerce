6109057423f5c72987969fe39d8757ee03b4f2a3
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-24 19:31:20 -0300

    Return one product

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -2,8 +2,22 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateProductService from '@modules/products/services/CreateProductService';
+import FindProductService from '@modules/products/services/FindProductService';
 
 export default class ProductsController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { current_url } = request;
+    const { id } = request.params;
+
+    const findProduct = container.resolve(FindProductService);
+    const product = await findProduct.execute({ id });
+
+    return response.json({
+      ...product,
+      url: current_url,
+    });
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, price, quantity } = request.body;
 
--- src/modules/products/infra/http/routes/products.routes.ts
+++ src/modules/products/infra/http/routes/products.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import ProductsController from '../controller/ProductsController';
-import ProductValidator from '../validators/ProductValidator';
+import IdValidator from '@shared/infra/http/validators/IdValidator';
+import ProductsController from '@modules/products/infra/http/controller/ProductsController';
+import ProductValidator from '@modules/products/infra/http/validators/ProductValidator';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.post('/', ProductValidator, productsController.create);
+productsRouter.get('/:id', IdValidator, productsController.show);
 
 export default productsRouter;
